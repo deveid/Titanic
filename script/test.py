244b2b32b1f8395972057f3373f9e6278b06ce06
deveid <deveidtolu18@gmail.com>
2023-11-28 19:15:34 -0400

    Update test.py

--- script/test.py
+++ script/test.py
@@ -4,7 +4,7 @@ class model():
   def __init__(self):
     self.d = {'col1': [1, 2], 'col2': [3, 4]}
   
-  def get_rec(self):
+  def get_rec():
     df = pd.DataFrame(data=self.d)
     df.to_markdown('fer.md',index=False)
 
